25cf4c9df66cd84ed916ae870f757217af9eb494
Gaurav Mehta <gaurav.mehta@suse.com>
2025-12-12 15:13:56 +1100

    Added an additional field PowerState in InventoryStatus and additional logic to reconcile Machine PowerState changes into Inventory status
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>
    
    include feedback from PR review
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>

--- pkg/controllers/inventory_controller_test.go
+++ pkg/controllers/inventory_controller_test.go
@@ -355,3 +355,140 @@ var _ = Describe("update inventory spec and reconcile machine spec", func() {
 		}).ShouldNot(HaveOccurred())
 	})
 })
+
+var _ = Describe("reconcile out of band machine power operation", func() {
+	var i *seederv1alpha1.Inventory
+	var creds *corev1.Secret
+
+	BeforeEach(func() {
+		i = &seederv1alpha1.Inventory{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "out-of-band-sample",
+				Namespace: "default",
+			},
+			Spec: seederv1alpha1.InventorySpec{
+				PrimaryDisk:                   "/dev/sda",
+				ManagementInterfaceMacAddress: "xx:xx:xx:xx:xx",
+				BaseboardManagementSpec: rufio.MachineSpec{
+					Connection: rufio.Connection{
+						Host:        "localhost",
+						Port:        623,
+						InsecureTLS: true,
+						AuthSecretRef: corev1.SecretReference{
+							Name:      "out-of-band-sample",
+							Namespace: "default",
+						},
+					},
+				},
+			},
+		}
+
+		creds = &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "out-of-band-sample",
+				Namespace: "default",
+			},
+			StringData: map[string]string{
+				"username": "admin",
+				"password": "password",
+			},
+		}
+
+		Eventually(func() error {
+			err := k8sClient.Create(ctx, creds)
+			if err != nil {
+				return err
+			}
+			err = k8sClient.Create(ctx, i)
+			return err
+		}, "30s", "5s").ShouldNot(HaveOccurred())
+	})
+
+	It("perform inventory and machine reconciles", func() {
+		By("check inventory reconcile", func() {
+			Eventually(func() error {
+				iObj := &seederv1alpha1.Inventory{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Namespace: i.Namespace, Name: i.Name}, iObj)
+				if err != nil {
+					return err
+				}
+
+				if iObj.Status.Status != seederv1alpha1.InventoryReady {
+					return fmt.Errorf("waiting for baseboard object to be created. Current status %v", iObj)
+				}
+				return nil
+			}, "30s", "5s").ShouldNot(HaveOccurred())
+		})
+
+		By("reconcile baseboard spec", func() {
+			Eventually(func() error {
+				b := &rufio.Machine{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Namespace: i.Namespace, Name: i.Name}, b)
+				if err != nil {
+					return err
+				}
+
+				if reflect.DeepEqual(i.Spec.BaseboardManagementSpec, b.Spec) {
+					return nil
+				}
+
+				return fmt.Errorf("waiting for baseboard spec to match inventory baseboard spec")
+			}, "30s", "5s").ShouldNot(HaveOccurred())
+		})
+
+		for _, expectedPowerState := range []rufio.PowerState{rufio.On, rufio.Unknown, rufio.Off} {
+			By("update machine power state", func() {
+				Eventually(func() error {
+					b := &rufio.Machine{}
+					err := k8sClient.Get(ctx, types.NamespacedName{Namespace: i.Namespace, Name: i.Name}, b)
+					if err != nil {
+						return err
+					}
+					b.Status.Power = expectedPowerState
+					return k8sClient.Status().Update(ctx, b)
+				}).ShouldNot(HaveOccurred())
+			})
+
+			By("reconcile inventory machine power state", func() {
+				Eventually(func() error {
+					iObj := &seederv1alpha1.Inventory{}
+					err := k8sClient.Get(ctx, types.NamespacedName{Namespace: i.Namespace, Name: i.Name}, iObj)
+					if err != nil {
+						return err
+					}
+
+					if iObj.Status.MachinePowerState != expectedPowerState {
+						return fmt.Errorf("waiting for inventory machine power state to be updated. Current state %v", iObj.Status.MachinePowerState)
+					}
+
+					return nil
+				}, "30s", "5s").ShouldNot(HaveOccurred())
+			})
+		}
+
+	})
+
+	AfterEach(func() {
+		Eventually(func() error {
+			return k8sClient.Delete(ctx, creds)
+
+		}).ShouldNot(HaveOccurred())
+
+		Eventually(func() error {
+			return k8sClient.Delete(ctx, i)
+
+		}).ShouldNot(HaveOccurred())
+
+		Eventually(func() error {
+			// wait until finalizers have cleaned up objects
+			err := k8sClient.Get(ctx, types.NamespacedName{Namespace: i.Namespace, Name: i.Name}, i)
+			if err != nil {
+				// object is missing
+				if apierrors.IsNotFound(err) {
+					return nil
+				}
+			}
+			return fmt.Errorf("waiting for inventory object to be not found")
+		}).ShouldNot(HaveOccurred())
+	})
+})
